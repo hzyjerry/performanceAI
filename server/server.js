041df39477a26e891f6eb082c8805e15a844a26e
Zhiyang He <zhiyanghe96@gmail.com>
2016-07-14 13:21:46 -0700

    Server side rendering structure

--- server/server.js
+++ server/server.js
@@ -22,7 +22,6 @@ const port = 3000
 
 // Use this middleware to set up hot module reloading via webpack.
 const compiler = webpack(webpackConfig)
-// console.log(webpackConfig.output.publicPath)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
 
@@ -30,6 +29,51 @@ app.use(Express.static(__dirname + '/../client'));
 
 // This is fired every time the server side receives a request
 app.use(handleRender)
+
+const HTML = ({ content, store }) => (
+  <html>
+    <head>
+      <meta charset="utf-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <meta name="description" content=""/>
+      <meta name="author" content=""/>
+
+      <title>Performance AI - Automatic Performance Analyzer</title>
+
+      <link href="css/bootstrap.min.css" rel="stylesheet"/>
+      {/* Custom CSS */}
+      <link href="css/sb-admin.css" rel="stylesheet"/>
+      {/* Morris Charts CSS */}
+      <link href="css/plugins/morris.css" rel="stylesheet"/>
+      {/* Custom Fonts */}
+      <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+      {/* 
+      HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries
+      WARNING: Respond.js doesn't work if you view the page via file://
+        [if lt IE 9]
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        [endif]
+      */}
+    </head>
+    <body>
+      <div id="wrapper">
+      </div>
+      <script dangerouslySetInnerHTML={{ __html: `window.__initialState__=${JSON.stringify(store.getState())};` }}/>
+
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      {/* Morris Charts JavaScript */}
+      <script src="js/plugins/morris/raphael.min.js"></script>
+      <script src="js/plugins/morris/morris.min.js"></script>
+      <script src="js/plugins/morris/morris-data.js"></script>
+      {/* Project Compile */}
+      <script src="/static/bundle.js"></script>
+    </body>
+  </html>
+)
 
 function handleRender(req, res) {
   // Query our mock API asynchronously
@@ -39,84 +83,15 @@ function handleRender(req, res) {
     const counter = parseInt(params.counter, 10) || apiResult || 0
     const upload = false
 
-    // Compile an initial state
     const preloadedState = { counter,
                              upload}
-
-    // Create a new Redux store instance
     const store = configureStore(preloadedState)
 
-    // Render the component to a string
-    const html = ""
-    /*renderToString(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    )*/
+    // For creating routerContext
+    const content = ""//renderToString()
 
-    // Grab the initial state from our Redux store
-    const finalState = store.getState()
-
-    // Send the rendered page back to the client
-    res.send(renderFullPage(html, finalState))
+    res.send('<!doctype html>\n' + renderToString(<HTML content={content} store={store}/>))
   })
-}
-
-function renderFullPage(html, preloadedState) {
-  return `
-    <!doctype html>
-    <html>
-      <head>
-
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <title>Performance AI - Automatic Performance Analyzer</title>
-
-        <!-- Bootstrap Core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="css/sb-admin.css" rel="stylesheet">
-
-        <!-- Morris Charts CSS -->
-        <link href="css/plugins/morris.css" rel="stylesheet">
-
-        <!-- Custom Fonts -->
-        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-      </head>
-      <body>
-        <div id="wrapper">
-        </div>
-        <script>
-          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
-        </script>
-        <!-- jQuery -->
-        <script src="js/jquery.js"></script>
-
-        <!-- Bootstrap Core JavaScript -->
-        <script src="js/bootstrap.min.js"></script>
-
-        <!-- Morris Charts JavaScript -->
-        <script src="js/plugins/morris/raphael.min.js"></script>
-        <script src="js/plugins/morris/morris.min.js"></script>
-        <script src="js/plugins/morris/morris-data.js"></script>
-
-        <!-- Project Compile -->
-        <script src="/static/bundle.js"></script>
-      </body>
-    </html>
-    `
 }
 
 app.listen(port, (error) => {

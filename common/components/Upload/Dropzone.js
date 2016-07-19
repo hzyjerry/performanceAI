f69d4cf4cab80d7ef0dfaf5ef072eb73b24a0f60
Zhiyang He <zhiyanghe96@gmail.com>
2016-07-19 16:07:26 -0700

    Delete button for dropzone upload

--- common/components/Upload/Dropzone.js
+++ common/components/Upload/Dropzone.js
@@ -20,24 +20,47 @@ var DropzoneDemo = React.createClass({
       this.refs.dropzone.open();
     },
 
+    attachDelete: function(file) {
+      self = this
+      return function() {
+        var index = self.state.files.findIndex(function(f) {
+          return f.name === file.name
+        })
+        console.log(index)
+        var newFiles = self.state.files
+        if (index > -1) {
+            newFiles.splice(index, 1);
+        }
+        self.setState({
+          files: newFiles
+        });
+      }
+    },
+
     render: function () {
+      self = this
+
       return (
         <div className="row">
           <Dropzone onDrop={this.onDrop} style={{
             width: '100%',
             align: 'center',
             height: '30px',
-            'border-width': '1px',
-            'border-color': "#666",
-            'border-style':"dashed",
-            'border-radius':"2px"
+            borderWidth: '1px',
+            borderColor: "#666",
+            borderStyle:"dashed",
+            borderRadius:"2px"
           }}>
             <div>{"  Try dropping some files here, or click to select files to upload."}</div>
           </Dropzone>
           <div className="col-xs-8">
             <ul>
               {this.state.files.map(function(file) {
-                return <li> {file.name}</li>
+                return (
+                  <li> {file.name}
+                    <button type="button" className="btn btn-xs btn-danger" onClick={self.attachDelete(file)}>Delete</button>
+                  </li>
+                )
               })} 
             </ul>
           </div>
